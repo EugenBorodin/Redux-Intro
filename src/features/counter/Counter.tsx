40e38b26251050f629577dfdf4e595d9f413365b
Eugen Borodin <Azsalatay@gmail.com>
2023-06-12 14:28:23 +0200

    first commit

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux"
+import Action from "./types/Action";
+import { RootState } from "../../store";
+
+
+export default function Counter(): JSX.Element {
+    const dispatch = useDispatch();
+    const value = useSelector((globalState: RootState) => globalState.counter.value)
+    return (
+        <div>
+            <button type="button"
+            onClick={() => dispatch<Action>({type: 'counter/minus', payload: 1})}>
+                -1
+            </button>{' '}
+            {value}{''}
+            <button type="button"
+            onClick={() => dispatch<Action>({type: 'counter/plus', payload: 1})}>
+                +1
+            </button>
+        </div>
+    )
+}
